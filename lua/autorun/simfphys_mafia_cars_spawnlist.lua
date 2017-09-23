0effb68cabb5eb771f4aa6e3bd7442e5f6c22021
def-valerius <31399259+def-valerius@users.noreply.github.com>
2017-09-23 10:48:47 +0500

    entity
    - Silver Fletcher

--- lua/autorun/simfphys_mafia_cars_spawnlist.lua
+++ lua/autorun/simfphys_mafia_cars_spawnlist.lua
@@ -5218,3 +5218,122 @@ local V = {
 	}
 }
 list.Set( "simfphys_vehicles", "simfphys_mafia_polimtud", V )
+
+local V = {
+	Name = "Silver Fletcher",
+	Model = "models/mafia/arrow.mdl",
+	Class = "gmod_sent_vehicle_fphysics_base",
+	Category = "Mafia",
+	SpawnOffset = Vector(0,0,10),
+	SpawnAngleOffset = 90,
+
+	Members = {
+		Mass = 2314,
+		
+		EnginePos = Vector(50,0,28),
+		
+		LightsTable = "arrow",
+		
+		CustomWheels = true,
+		CustomSuspensionTravel = 5,
+		
+		CustomWheelModel = "models/mafia/arrow_wheel.mdl",
+		CustomWheelPosFL = Vector(87,32,2),
+		CustomWheelPosFR = Vector(87,-34,2),
+		CustomWheelPosRL = Vector(-64,32,2),
+		CustomWheelPosRR = Vector(-64,-34,2),
+		CustomWheelAngleOffset = Angle(0,90,0),
+		
+		CustomMassCenter = Vector(10,0,10),
+		
+		CustomSteerAngle = 30,
+		
+		SeatOffset = Vector(-13,-15,43),
+		SeatPitch = 5,
+		SeatYaw = 90,
+		
+		PassengerSeats = {
+			{
+				pos = Vector(0,-14,12),
+				ang = Angle(0,-90,15)
+			},
+			{
+				pos = Vector(-45,-12,9),
+				ang = Angle(0,-90,15)
+			},
+			{
+				pos = Vector(-45,12,9),
+				ang = Angle(0,-90,15)
+			}
+		},
+		
+		ExhaustPositions = {
+			{
+				pos = Vector(-70,-20,0),
+				ang = Angle(90,165,0),
+			}
+		},
+		
+		FrontHeight = 6,
+		FrontConstant = 33000,
+		FrontDamping = 1000,
+		FrontRelativeDamping = 1000,
+		
+		RearHeight = 4,
+		RearConstant = 33000,
+		RearDamping = 1000,
+		RearRelativeDamping = 1000,
+		
+		FastSteeringAngle = 10,
+		SteeringFadeFastSpeed = 535,
+		
+		TurnSpeed = 5,
+		
+		MaxGrip = 37,
+		Efficiency = 1,
+		GripOffset = -2,
+		BrakePower = 25,
+		
+		IdleRPM = 700,
+		LimitRPM = 5400,
+		PeakTorque = 160,
+		PowerbandStart = 850,
+		PowerbandEnd = 5200,
+		Turbocharged = false,
+		Supercharged = false,
+		
+		FuelFillPos = Vector(-70,0,30),
+		FuelType = FUELTYPE_PETROL,
+		FuelTankSize = 38,
+		
+		PowerBias = 1,
+		
+		EngineSoundPreset = 0,
+		--
+		Sound_Idle = "mafia/arrow_x.wav",
+		Sound_IdlePitch = 1,
+		
+		Sound_Mid = "mafia/arrow_1.wav",
+		Sound_MidPitch = 1,
+		Sound_MidVolume = 6,
+		Sound_MidFadeOutRPMpercent = 90,
+		Sound_MidFadeOutRate = 0.9,
+		
+		Sound_High = "mafia/arrow_2.wav",
+		Sound_HighPitch = 1,
+		Sound_HighVolume = 1,
+		Sound_HighFadeInRPMpercent = 90,
+		Sound_HighFadeInRate = 0.9,
+		
+		Sound_Throttle = "mafia/arrow_3.wav",
+		Sound_ThrottlePitch = 1,
+		Sound_ThrottleVolume = 6,
+		
+		--
+		snd_horn = "mafia/horn14.wav",
+		
+		DifferentialGear = 0.62,
+		Gears = {-0.15,0,0.13,0.26,0.42}
+	}
+}
+list.Set( "simfphys_vehicles", "simfphys_mafia_arrow", V )
--- lua/autorun/simfphys_mafia_ligths.lua
+++ lua/autorun/simfphys_mafia_ligths.lua
@@ -2083,3 +2083,55 @@ local light_table = {
 
 }
 list.Set( "simfphys_lights", "polimtud", light_table)
+
+local light_table = {
+	L_HeadLampPos = Vector( 108, 29, 28 ),
+	L_HeadLampAng = Angle(0,0,0),
+	R_HeadLampPos = Vector( 108, -29, 28 ),
+	R_HeadLampAng = Angle(0,0,0),
+	
+	L_RearLampPos = Vector(-96,32,19),
+	L_RearLampAng = Angle(0,0,0),
+	R_RearLampPos = Vector(-96,-32,19),
+	R_RearLampAng = Angle(0,0,0),
+	
+	Headlight_sprites = {
+		{pos = Vector( 116, 12, 15 ),material = "sprites/light_ignorez",size = 100, color = Color( 255,200,150,160)},
+		{pos = Vector( 116, -12, 15 ),material = "sprites/light_ignorez",size = 100, color = Color( 255,200,150,160)},
+		
+	},
+	Headlamp_sprites = {
+		{pos = Vector( 108, 29, 28 ),material = "sprites/light_ignorez",size = 90, color = Color( 220,205,160,170)},
+		{pos = Vector( 108, -29, 28 ),material = "sprites/light_ignorez",size = 90, color = Color( 220,205,160,170)},
+	},
+	Rearlight_sprites = {
+		{pos = Vector(-96,32,19),material = "sprites/light_ignorez",size = 45,color = Color( 255, 0, 0,  150)},
+		{pos = Vector(-96,-32,19),material = "sprites/light_ignorez",size = 45,color = Color( 255, 0, 0,  150)},
+	},
+	Brakelight_sprites = {
+		{pos = Vector(-96,32,19),material = "sprites/light_ignorez",size = 45,color = Color( 255, 0, 0,  150)},
+		{pos = Vector(-96,-32,19),material = "sprites/light_ignorez",size = 45,color = Color( 255, 0, 0,  150)},
+	},
+	Reverselight_sprites = {
+		{pos = Vector(-96,30,19),material = "sprites/light_ignorez",size = 45,color = Color( 205, 200, 200, 150)},
+		{pos = Vector(-96,-30,19),material = "sprites/light_ignorez",size = 45,color = Color( 205, 200, 200,  150)},
+	},
+	
+	DelayOn = 0.1,
+	DelayOff = 0.1,
+	
+	Turnsignal_sprites = {
+		Left = {
+			Vector(-96,32,19),
+			{pos = Vector(-96,32,19),material = "sprites/light_ignorez",size = 55,color = Color( 255, 0, 0,  165)},
+			{pos = Vector( 116, 12, 15 ),material = "sprites/light_ignorez",size = 55,color = Color( 255, 150, 0,  165)},
+		},
+		Right = {
+			Vector(-96,-32,19),
+			{pos = Vector(-96,-32,19),material = "sprites/light_ignorez",size = 55,color = Color( 255, 0, 0,  165)},
+			{pos = Vector( 116, -12, 15 ),material = "sprites/light_ignorez",size = 55,color = Color( 255, 150, 0,  165)},
+		},
+	}
+
+}
+list.Set( "simfphys_lights", "arrow", light_table)
