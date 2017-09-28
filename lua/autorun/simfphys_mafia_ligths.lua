cc230898c1f94c61e52de2298ae284d394a5f959
def-valerius <31399259+def-valerius@users.noreply.github.com>
2017-09-28 14:18:30 +0500

    entity
    - Bolt-Thrower

--- lua/autorun/simfphys_mafia_ligths.lua
+++ lua/autorun/simfphys_mafia_ligths.lua
@@ -3046,3 +3046,56 @@ local light_table = {
 
 }
 list.Set( "simfphys_lights", "hotrodp2", light_table)
+
+local light_table = {
+	L_HeadLampPos = Vector( 110, 35, 26 ),
+	L_HeadLampAng = Angle(0,0,0),
+	R_HeadLampPos = Vector( 110, -35, 26 ),
+	R_HeadLampAng = Angle(0,0,0),
+	
+	L_RearLampPos = Vector(-115,33,25),
+	L_RearLampAng = Angle(0,0,0),
+	R_RearLampPos = Vector(-115,-33,25),
+	R_RearLampAng = Angle(0,0,0),
+	
+	Headlight_sprites = {
+		{pos = Vector( 110, 35, 26 ),material = "sprites/light_ignorez",size = 70, color = Color( 220,205,160,120)},
+		{pos = Vector( 110, -35, 26 ),material = "sprites/light_ignorez",size = 70, color = Color( 220,205,160,120)},
+		
+	},
+	Headlamp_sprites = {
+		{pos = Vector( 110, 35, 26 ),material = "sprites/light_ignorez",size = 100, color = Color( 220,205,160,150)},
+		{pos = Vector( 110, -35, 26 ),material = "sprites/light_ignorez",size = 100, color = Color( 220,205,160,150)},
+		},
+	Rearlight_sprites = {
+		{pos = Vector(-115,33,25),material = "sprites/light_ignorez",size = 60,color = Color( 255, 0, 0,  150)},
+		{pos = Vector(-115,-33,25),material = "sprites/light_ignorez",size = 60,color = Color( 255, 0, 0,  150)},
+	},
+	Brakelight_sprites = {
+		{pos = Vector(-115,33,25),material = "sprites/light_ignorez",size = 45,color = Color( 255, 0, 0,  150)},
+		{pos = Vector(-115,-33,25),material = "sprites/light_ignorez",size = 45,color = Color( 255, 0, 0,  150)},
+	},
+	Reverselight_sprites = {
+		{pos = Vector(-115,33,25),material = "sprites/light_ignorez",size = 25,color = Color( 255, 255, 255, 150)},
+		{pos = Vector(-115,-33,25),material = "sprites/light_ignorez",size = 25,color = Color( 255, 255, 255, 150)},
+	},
+	
+	DelayOn = 0.1,
+	DelayOff = 0.1,
+	
+	Turnsignal_sprites = {
+		Left = {
+			Vector(-115,36,25),
+			Vector( 106, 35, 11 ),
+			{pos = Vector(-115,36,25),material = "sprites/light_ignorez",size = 55,color = Color( 255, 0, 0,  165)},
+		},
+		Right = {
+			Vector(-115,-36,25),
+			Vector( 106, -35, 11 ),
+			{pos = Vector(-115,-36,25),material = "sprites/light_ignorez",size = 55,color = Color( 255, 0, 0,  165)},
+		},
+	}
+	
+
+}
+list.Set( "simfphys_lights", "Thunderbird", light_table)
